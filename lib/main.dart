4d792d0bdcb23a0587e2b34f864485a626949bf3
bujosa <davidbujosa@gmail.com>
2023-05-16 01:51:30 -0400

    feat: finish cloud messaging integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
   runApp(const MyApp());
 }
 
